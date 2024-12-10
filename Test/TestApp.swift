cd675e18f9a9cac6488505b0ceb7d77aeb2bab29
Marziap <marziapirozzi2002@gmail.com>
2024-12-10 12:00:17 +0100

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Marzia Pirozzi on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
