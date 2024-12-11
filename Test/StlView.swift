0ddb32be69d726628cdfc6548be21043dfba7a4e
Marziap <marziapirozzi2002@gmail.com>
2024-12-11 12:26:00 +0100

    rotate high poly model

--- Test/StlView.swift
+++ Test/StlView.swift
@@ -4,7 +4,7 @@ import RealityKit
 struct StlView: View {
     var body: some View {
         VStack(spacing: 18.0) {
-            Model3D(named: "ImageToStl.com_iso") { model in
+            Model3D(named: "modelPyvista3") { model in
                 switch model {
                 case .empty:
                     ProgressView()
@@ -13,9 +13,9 @@ struct StlView: View {
                     resolvedModel3D
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(height: 200)
-                        .rotation3DEffect(.degrees(100), axis: (x: 1, y: 0, z: 0)) // Rotate 45° around the X-axis
-                        .rotation3DEffect(.degrees(0), axis: (x: 0, y: 1, z: 0)) // Rotate 30° around the Y-axis
+                        .frame(minHeight: 200)
+                        .rotation3DEffect(.degrees(90), axis: (x: 1, y: 0, z: 0)) // Rotate 45° around the X-axis
+                        .rotation3DEffect(.degrees(270), axis: (x: 0, y: 0, z: 1)) // Rotate 30° around the Y-axis
                         
                 case .failure(let error):
                     Text(error.localizedDescription)
